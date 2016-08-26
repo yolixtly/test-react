aecb5ec3fed6bd6a81800439b9605a378957f0bb
yolixtly <yolixtly_13@hotmail.com>
2016-08-26 09:27:23 -0400

    image test pass

--- client/js/gallery.js
+++ client/js/gallery.js
@@ -1,8 +1,10 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 
 var Image = require('./image');
 
 var Gallery = function(props) {
+	console.log(props.images);
     var images = props.images.map(function(image, index) {
         return <Image url={image.url} description={image.description} key={index}/>;
     });
@@ -12,4 +14,19 @@ var Gallery = function(props) {
     );
 };
 
+var collection = [
+{
+	url: "http://pre04.deviantart.net/9e11/th/pre/f/2014/082/8/2/pokemon_y__eevee_by_smiley_fakemon-d7bbnee.png",
+	description: "Eevee pokemon"	
+},
+{
+	url: "https://1.bp.blogspot.com/-Px--GuqYC7Y/V2f6nNxOvrI/AAAAAAAAEKg/FcxJWpM1UTEKgn7h6ijAEJWSkMIvTloSACLcB/s1600/power_of_the_bunny.jpg",
+	description: "Virtual Reality Bunny!"
+}
+];
+
 module.exports = Gallery;
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(<Gallery  images={collection}/>, document.getElementById('app'));
+});
--- client/js/image.js
+++ client/js/image.js
@@ -3,10 +3,11 @@ var React = require('react');
 var Image = function(props) {
     return (
         <div className="gallery-image">
-            <img src={props.url} alt={props.description} />
+            <img className="style-image" src={props.url} alt={props.description} />
             <p>{props.description}</p>
         </div>
     );
 };
 
 module.exports = Image;
+
